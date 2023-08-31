5ad900a9dcdcb6181eb715f6ebea9d8c7594869d
Lucas vinicius alencar alves <lucasviniciusaalves@rsa.bet>
2023-08-31 09:44:19 -0300

    update: Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import { Inter } from 'next/font/google'
 import { Providers } from './providers'
 import { Sidebar } from '@/components/Sidebar'
 import { usePathname } from 'next/navigation'
-import { useAuth } from '@/hooks/useAuth'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,19 +15,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   const pathname = usePathname()
-  const { user } = useAuth()
 
   return (
     <html lang="en">
       <body className={inter.className}>
         <Providers>
           <div className="bg-[#3d3d3d]">
-            {user?.id && <Header />}
-            <div
-              className={`mx-auto flex ${
-                user?.id ? 'h-[calc(100vh-80px)]' : 'h-screen'
-              } `}
-            >
+            <Header />
+            <div className="mx-auto flex h-[calc(100vh-80px)]">
               {pathname.split('/')[1] !== 'sign-in' && <Sidebar />}
               <div className="w-full overflow-auto">{children}</div>
             </div>
